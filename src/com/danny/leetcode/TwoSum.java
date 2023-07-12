6fa7f4721e6b2369387032487afb74a762e0a353
吳東祐 <dannywu00865@intumit.com>
2023-07-13 00:51:03 +0800

    finshed two sum.

--- src/com/danny/leetcode/TwoSum.java
+++ src/com/danny/leetcode/TwoSum.java
@@ -1,10 +1,24 @@
 package com.danny.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
+	public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int res = target - num;
+            if(numMap.containsKey(res)) {
+                return new int[]{numMap.get(res), i};
+            }
+            numMap.put(num, i);
+        }
+        return new int[] {};
+    }
 }
